5a73fdd7023c5421c5eb693d2d9a66643d4ce9ae
Felix Antoni <felix.antoni@p92.hu>
2020-09-07 20:28:39 +0200

    Added few lines to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Main {
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import java.io.IOException;
+
+public class Main {
+    HttpRequestFactory requestFactory = new NetHttpTransport().createRequestFactory();
+    HttpRequest request = requestFactory.buildGetRequest()
+
+    public Main() throws IOException {
+    }
 }

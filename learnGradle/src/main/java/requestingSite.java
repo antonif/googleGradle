bf387168e1ed625a6f839cd1f2f8ff45a6461585
antonif <32664236+antonif@users.noreply.github.com>
2020-09-07 20:33:03 +0200

    Delete requestingSite.java

--- learnGradle/src/main/java/requestingSite.java
+++
@@ -1,8 +0,0 @@
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-public class requestingSite {
-    URL url = new URL("http://google.com");
-    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-    con.setRequestMethod("GET");
-}

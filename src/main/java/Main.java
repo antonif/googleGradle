236d5d75afa44635d984f933274b7204f7313276
Felix Antoni <felix.antoni@p92.hu>
2020-09-08 13:55:25 +0200

    .gradle added to .gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,22 @@
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpResponse;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
         System.out.println("What's your name: ");
         String name = scan.nextLine();
         classToTest.writeName(name);
+
+        //making simple request
+        HttpRequestFactory requestFactory = new NetHttpTransport().createRequestFactory();
+        HttpRequest request = requestFactory.buildGetRequest(new GenericUrl("https://github.com"));
+        HttpResponse rawResponse = request.execute();
+        System.out.println(rawResponse.getHeaders());
     }
 }
